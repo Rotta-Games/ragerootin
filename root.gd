ec38345c2871130a555a69f4f32d3325700d9463
Pete Sevander <pete.sevander@gmail.com>
2023-02-04 13:26:24 +0200

    root refactoring

--- root.gd
+++ root.gd
@@ -1,7 +1,10 @@
 extends Node2D
 
 @onready var line = $Line2D
+@onready var body = $Line2D/BodyArea
+@onready var head = $Line2D/HeadArea
 
+var alive = true
 var angle = 10
 var speed = 100
 var turn_speed = 10
@@ -9,22 +12,23 @@ var head_pos = Vector2(0, 0)
 var head_dir = Vector2(1, 0)
 
 func _ready():
-	head_pos = self.get_position()
-	add_point(line, head_pos)
-	add_point(line, head_pos)
+	head_pos = self.position
+	add_point(head_pos)
+	add_point(head_pos)
 
 
 func _process(delta):
+	if not alive:
+		return
 	var rotated = head_dir.rotated(angle * delta)
 	head_pos += rotated * speed * delta
-	move_last_point(line, head_pos)
+	move_last_point(head_pos)
 
 
-func add_point(line, point):
+func add_point(point):
 	var last_point = line.points[-1]
 	line.add_point(point)
 
-	var body = line.get_node("Area2D")
 	var shape = CollisionShape2D.new()
 	var rect = RectangleShape2D.new()
 
@@ -38,11 +42,11 @@ func add_point(line, point):
 	body.add_child(shape)
 
 
-func move_last_point(line: Line2D, point: Vector2):
+func move_last_point(point: Vector2):
 	line.set_point_position(line.points.size()-1, point)
+	head.position = point
 	var second_last = line.points[-2]
 
-	var body = line.get_node("Area2D")
 	var shape = body.get_child(-1)
 	var rect = shape.shape
 
@@ -54,4 +58,4 @@ func move_last_point(line: Line2D, point: Vector2):
 
 
 func _on_segment_timer_timeout():
-	add_point(line, head_pos)
+	add_point(head_pos)
