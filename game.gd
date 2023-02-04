f58aa39b7f2aece02ebd12716029b9e6df0a7161
Pete Sevander <pete.sevander@gmail.com>
2023-02-04 20:13:49 +0200

    Print score

--- game.gd
+++ game.gd
@@ -3,7 +3,13 @@ extends Node2D
 @onready var player1 = $Player1
 @onready var player2 = $Player2
 
+@onready var planet = $Planet
+
 func _ready():
-	pass
+	planet.connect("planet_dry", self.winrar, CONNECT_ONE_SHOT)
+
+func winrar():
+	print("player1 score: ", player1.score)
+	print("player2 score: ", player2.score)
 
 
