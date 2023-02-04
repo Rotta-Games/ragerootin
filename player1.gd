ced2c26bdec9ae6aaa3b606cbe1b5c620b62cbf5
Joonas Reinikka <joonas.reinikka@gmail.com>
2023-02-04 16:28:22 +0200

    fix player inputs

--- player1.gd
+++ player1.gd
@@ -5,7 +5,7 @@ extends "res://player.gd"
 func _ready():
 	pass # Replace with function body.
 
-func _input(event):
+func _physics_process(delta):
 	if Input.is_action_pressed("player1_move_left"):
 		root.angle += -root.turn_speed
 	if Input.is_action_pressed("player1_move_right"):
