57c6864faebbee7398a73354d67c7c33e86c41c1
Teemu Risikko <teemu.risikko@siili.com>
2023-02-05 12:13:52 +0200

    Change end screen texture based on winner

--- game.gd
+++ game.gd
@@ -9,6 +9,11 @@ func _ready():
 	planet.connect("planet_dry", self.winrar, CONNECT_ONE_SHOT)
 
 func winrar():
+	if(GameState.player1_water > GameState.player2_water):
+		GameState.winner_texture = $Planet/Tree1.texture
+	else:
+		GameState.winner_texture = $Planet/Tree2.texture
+	
 	get_tree().change_scene_to_file("res://game_over.tscn")
 
 
