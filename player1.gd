5caac6255458989231e04d42fd64c186e08df848
Pete Sevander <pete.sevander@gmail.com>
2023-02-05 10:38:48 +0200

    Fix player2 gamepad input

--- player1.gd
+++ player1.gd
@@ -16,6 +16,6 @@ func _physics_process(delta):
 			root.angle += root.turn_speed * delta
 
 func _input(event):
-	if Input.is_action_pressed("player1_shoot"):
+	if Input.is_action_just_pressed("player1_shoot"):
 		self.set_start_position(sliderButton.get_global_position())
 		shoot()
